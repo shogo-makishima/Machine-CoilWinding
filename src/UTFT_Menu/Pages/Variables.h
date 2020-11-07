76cafc5699f4b3ce48dca1f3b6b0b4fb585313b9
shogo-makishima <segor148@gmail.com>
2020-11-07 15:06:15 +0300

    modified:   .pio/build/project.checksum 	modified:   .vscode/c_cpp_properties.json 	modified:   .vscode/launch.json 	modified:   platformio.ini 	modified:   src/CoilWinding/CoilWinding.hpp 	modified:   src/UTFT_Menu/Pages/MainPage.h 	modified:   src/UTFT_Menu/Pages/SpeedControlPage.h 	modified:   src/UTFT_Menu/Pages/Variables.h 	modified:   src/main.cpp

--- src/UTFT_Menu/Pages/Variables.h
+++ src/UTFT_Menu/Pages/Variables.h
@@ -12,31 +12,4 @@ void ChangeVCounter() {
 
 static bool VMode = true;
 
-static bool VFirstLoad = true;
-static long int VSpeedINT = 0; // size = 4 byte
-
-static int VSpeed[6] = { 0, 0, 0, 0, 0, 0, };
-// int EEMEM VSpeed_ADDR[6];
-
-void EnterVSpeedInt() {
-    Serial.println("[VSPEED] { " + String(VSpeed[0]) + ", " + String(VSpeed[1]) + ", ... }");
-    VSpeedINT = VSpeed[0] * 100000 + VSpeed[1] * 10000 + VSpeed[2] * 1000 + VSpeed[3] * 100 + VSpeed[4] * 10 + VSpeed[5] * 1; 
-    Serial.println("[SPEED] " + String(VSpeedINT));
-}
-
-void ClearVSpeed() {
-    for (int i = 0; i < 6; i++) VSpeed[i] = 0;
-}
-
-void LoadVSpeedInt() {
-    Serial.println("[LOAD] VSpeed");
-    // eeprom_read_block((void*)&VSpeed, (const void*)&VSpeed_ADDR, sizeof(VSpeed));
-    EnterVSpeedInt();
-}
-
-void SaveVSpeedInt() {
-    Serial.println("[SAVE] VSpeed");
-    // eeprom_write_block((void*)&VSpeed, (const void*)&VSpeed_ADDR, sizeof(VSpeed));
-}
-
 #endif
